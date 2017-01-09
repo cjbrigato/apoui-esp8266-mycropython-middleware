8f4b0fc3746b3fbb50966e4dc846c8404a682875
Colin BRIGATO <colin.brigato@magic.fr>
2017-01-09 06:49:43 +0100

    new HAL #1

--- system.py
+++ system.py
@@ -1,29 +1,74 @@
+"""
+SystemOS Module
+"""
+import sys
 import machine
 import network
 import socket
+# import shell
+
+#__all__ = ["SystemOS"]
+
+# class SystemOS:
+
+
+class Kernel:
+
+    def __init__(self):
+        self.handle()
+
+    def system_release(self, build):
+        print(" .:::.   .:::.")
+        print(":::::::.:::::::")
+        print(":::::::::::::::   Cystick_Handlers(tm)")
+        print("':::::::::::::'    < APOUI-SYSTEMS >")
+        print("  ':::::::::'              - ")
+        print("    ':::::'  [Home middleware Controller]   ")
+        print("      ':'        build {0}".format(build))
+        print()
+
+
+    def boot(self, build):
+        self.system_release(build)
+        print(">> Booting KERNEL")
+        # Set it at 160Mhz for full speed
+        machine.freq(160000000)
+        # We should have a list of wifi we can try to connect to ?
+        #[i for i, v in enumerate(network.WLAN(network.STA_IF).scan()) if v[0] == 'excellency']
+        wifi = MicroWifi('APOUI', 'astis4-maledictio6-pultarius-summittite')
+        controller = ApouiControl('http://control.maison.apoui.net/setrelay')
+        print('>> Controller Tests...')
+        print("!> READY")
+        print()
+        return controller
+
+
+    def handle(self):
+        controller = self.boot('b1483918977')
+        shell = KernelShell(controller)
+        shell.cmdloop()
+        # try:
+        #    shell.cmdloop()
+        # except KeyboardInterrupt:
+        #    print("<< Received SIGINT from Keyboard")
+
+
+class KernelDriver:
+    pass
+
+
+class KernelTask:
+    pass
+
+
+class UserApp:
+    pass
 
 from cmd import Cmd
 
 
-class Kernel(object):
-    pass
-
-
-class KernelDriver(object):
-    pass
-
-
-class KernelTask(object):
-    pass
-
-
-class UserApp(object):
-    pass
-
-class KernelShell(object):
-    pass
-
-class ApouiShell(Cmd):
+class KernelShell(Cmd):
+    # class ApouiShell(Cmd):
 
     def __init__(self,  controller):
         super().__init__()
@@ -114,41 +159,3 @@ class MicroWifi:
             while not sta_if.isconnected():
                 pass
         print('OK')
-
-
-def system_release(build):
-    print(" .:::.   .:::.")
-    print(":::::::.:::::::")
-    print(":::::::::::::::   Cystick_Handlers(tm)")
-    print("':::::::::::::'    < APOUI-SYSTEMS >")
-    print("  ':::::::::'              - ")
-    print("    ':::::'  [Home middleware Controller]   ")
-    print("      ':'        build {0}".format(build))
-    print()
-
-
-def boot(build):
-    system_release(build)
-    print(">> Booting KERNEL")
-    # Set it at 160Mhz for full speed
-    machine.freq(160000000)
-    # We should have a list of wifi we can try to connect to ?
-    #[i for i, v in enumerate(network.WLAN(network.STA_IF).scan()) if v[0] == 'excellency']
-    wifi = MicroWifi('APOUI', 'astis4-maledictio6-pultarius-summittite')
-    controller = ApouiControl('http://control.maison.apoui.net/setrelay')
-    print('>> Controller Tests...')
-    print("!> READY")
-    print()
-    return controller
-
-
-def main():
-    controller = boot('b1483918977')
-    shell = ApouiShell(controller)
-    shell.cmdloop()
-    # try:
-    #    shell.cmdloop()
-    # except KeyboardInterrupt:
-    #    print("<< Received SIGINT from Keyboard")
-
-main()
